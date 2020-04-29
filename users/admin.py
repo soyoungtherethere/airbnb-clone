ae57231931414b5c075690ba0642da3fc77af304
soyoungtherethere <311syp@naver.com>
2020-04-29 02:31:04 -0700

    #3.5 UserAdmin + CustomAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,17 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
 
     """ Custom Use Admin """
 
-    list_display = ("username", "email", "gender", "language", "currency", "superhost")
-    list_filter = (
-        "superhost",
-        "language",
-        "currency",
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
     )
