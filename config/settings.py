049b6b799d81786db92caa1aff227e945806dbe2
soyoungtherethere <311syp@naver.com>
2020-04-29 02:53:03 -0700

    #3.6 Wrap up User App

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
+THIRD_PARTY_APPS = []
+
 PROJECT_APPS = [
     "users.apps.UsersConfig",
 ]
@@ -120,4 +122,3 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 AUTH_USER_MODEL = "users.User"
-
