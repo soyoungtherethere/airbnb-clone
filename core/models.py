91586bc3af4db3442d2f9faeb001f6547dd374b2
soyoungtherethere <311syp@naver.com>
2020-04-29 04:21:57 -0700

    #4.1 Room Model (1)
    building room models using django-countries package and ForeignKey

--- core/models.py
+++ core/models.py
@@ -7,8 +7,8 @@ class TimeStampedModel(models.Model):
 
     """ Time Stamped Model """
 
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        abstact = True
+        abstract = True
