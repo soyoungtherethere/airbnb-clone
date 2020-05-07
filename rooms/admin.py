922e4970766a0877a1fdc8630f3fc1664751a3aa
soyoungtherethere <311syp@naver.com>
2020-05-07 06:49:06 -0700

    #6.1 Room Admin Panel (2)
    creating fieldsets of Rooms App. classes collapse is so cool

--- rooms/admin.py
+++ rooms/admin.py
@@ -17,6 +17,23 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book",)}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths",)}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules",),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
     list_display = (
         "name",
         "country",
@@ -33,6 +50,11 @@ class RoomAdmin(admin.ModelAdmin):
 
     list_filter = (
         "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
         "city",
         "country",
     )
@@ -40,6 +62,12 @@ class RoomAdmin(admin.ModelAdmin):
     search_fields = (
         "=city",
         "^host__username",
+    )
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
     )
 
 
