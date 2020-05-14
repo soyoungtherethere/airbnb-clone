29c34bf7c5804d5a1eecf95d801945b89623ff1c
soyoungtherethere <311syp@naver.com>
2020-05-14 07:39:52 -0700

    #8.2 Conversations, Lists, Reservations Admin

--- lists/admin.py
+++ lists/admin.py
@@ -7,4 +7,12 @@ from . import models
 @admin.register(models.List)
 class ListAdmin(admin.ModelAdmin):
 
-    pass
+    list_display = (
+        "name",
+        "user",
+        "count_rooms",
+    )
+
+    search_fields = ("name",)
+
+    filter_horizontal = ("rooms",)
